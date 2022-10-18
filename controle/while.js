2d87671fed9f796ce250ce77f01c42fc0e244e35
igorjdias <igorjorge17@gmail.com>
2022-10-17 22:06:12 -0300

    aula-dia-17_10_2022

---
+++ controle/while.js
@@ -0,0 +1,14 @@
+//estrutura de repetição enquanto existir verdadeiro ou falso, quando for falsa ele sai do laço
+function getInteiroAleatorioEntre(min, max) {
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+
+let opcao = 0
+
+while (opcao != -1) {
+    opcao = getInteiroAleatorioEntre(-1, 10)
+    console.log(`Opção escolhida foi ${opcao}.`)
+}
+
+console.log('Até a próxima!')
