6ce408e489e3d8675c679cd8fd67b70e60f9eeac
igorjdias <igorjorge17@gmail.com>
2022-10-18 21:09:50 -0300

    AulaDia18-10-2022

---
+++ controle/doWhile.js
@@ -0,0 +1,14 @@
+// vai executar mesmo se chegar no resultado definido.
+function getInteiroAleatorioEntre(min, max) {
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+
+let opcao = -1
+
+do {
+    opcao = getInteiroAleatorioEntre(-1, 10)
+    console.log(`Opção escolhida foi ${opcao}.`)
+} while (opcao != -1) 
+
+console.log('Até a próxima!')
